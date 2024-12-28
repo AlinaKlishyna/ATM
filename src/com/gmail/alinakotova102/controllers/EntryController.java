86b2632385d869717db4e29a3c69c4163daf06a4
Alina <alinakotova102@gmail.com>
2024-12-28 20:03:33 +0100

    array to buttons with numbers update

--- src/com/gmail/alinakotova102/controllers/EntryController.java
+++ src/com/gmail/alinakotova102/controllers/EntryController.java
@@ -40,69 +40,28 @@ public class EntryController {
     @FXML
     private Button numZero;
 
-    private Integer pinCode;
-
     private final byte maxLength = 4;
-
-    public Integer getPinCode() {
-        return pinCode;
-    }
 
     @FXML
     void initialize() {
-        numOne.setOnAction(event -> {
-            if (checkTextLimiter()) {
-                authSingField.appendText("1");
-            }
-        });
-        numTwo.setOnAction(event -> {
-            if (checkTextLimiter()) {
-                authSingField.appendText("2");
-            }
-        });
-        numThree.setOnAction(event -> {
-            if (checkTextLimiter()) {
-                authSingField.appendText("3");
-            }
-        });
-        numFour.setOnAction(event -> {
-            if (checkTextLimiter()) {
-                authSingField.appendText("4");
-            }
-        });
-        numFive.setOnAction(event -> {
-            if (checkTextLimiter()) {
-                authSingField.appendText("5");
-            }
-        });
-        numSix.setOnAction(event -> {
-            if (checkTextLimiter()) {
-                authSingField.appendText("6");
-            }
-        });
-        numSeven.setOnAction(event -> {
-            if (checkTextLimiter()) {
-                authSingField.appendText("7");
-            }
-        });
-        numEight.setOnAction(event -> {
-            if (checkTextLimiter()) {
-                authSingField.appendText("8");
-            }
-        });
-        numNine.setOnAction(event -> {
-            if (checkTextLimiter()) {
-                authSingField.appendText("9");
-            }
-        });
-        numZero.setOnAction(event -> {
-            if (checkTextLimiter()) {
-                authSingField.appendText("0");
-            }
-        });
+        Button[] numbers = {
+                numZero, numOne, numTwo, numThree, numFour, numFive, numSix, numSeven, numEight, numNine};
+
+        for (int i = 0; i < numbers.length; i++) {
+            int num = i;
+            numbers[i].setOnAction(event -> {
+                if (checkTextLimiter())
+                    authSingField.appendText(String.valueOf(num));
+            });
+        }
     }
 
     boolean checkTextLimiter() {
         return authSingField.getCharacters().length() < maxLength;
+    }
+
+    @FXML
+    void submit(ActionEvent event) {
+
     }
 }
