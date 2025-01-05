097523ffac5fbb90dfd7553b0c8123557150ec06
Alina <alinakotova102@gmail.com>
2025-01-05 20:15:18 +0100

    method printLineByLine for print text to labels

--- src/com/gmail/alinakotova102/controllers/EntryController.java
+++ src/com/gmail/alinakotova102/controllers/EntryController.java
@@ -1,9 +1,15 @@
 package com.gmail.alinakotova102.controllers;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
 
 public class EntryController {
 
@@ -40,28 +46,48 @@ public class EntryController {
     @FXML
     private Button numZero;
 
+    @FXML
+    private Label startLabel;
+
     private final byte maxLength = 4;
 
     @FXML
     void initialize() {
+        printLineByLine("Pin Code", startLabel);
+
         Button[] numbers = {
                 numZero, numOne, numTwo, numThree, numFour, numFive, numSix, numSeven, numEight, numNine};
 
         for (int i = 0; i < numbers.length; i++) {
             int num = i;
             numbers[i].setOnAction(event -> {
-                if (checkTextLimiter())
+                if (checkSizeLimit(maxLength))
                     authSingField.appendText(String.valueOf(num));
             });
         }
     }
 
-    boolean checkTextLimiter() {
-        return authSingField.getCharacters().length() < maxLength;
+    public void printLineByLine(String text, Label output) {
+        Timeline timeline = new Timeline();
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(100), new EventHandler<ActionEvent>() {
+            int i = 0;
+
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if (i <= text.length()) {
+                    output.setText(text.substring(0, i));
+                    i++;
+                } else {
+                    timeline.stop();
+                }
+            }
+        });
+        timeline.getKeyFrames().add(keyFrame);
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
-    @FXML
-    void submit(ActionEvent event) {
-
+    boolean checkSizeLimit(int size) {
+        return authSingField.getCharacters().length() < size;
     }
 }
